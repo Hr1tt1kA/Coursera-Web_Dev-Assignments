13a74e9e12b33de57ddfb6362a11cc9f54556292
Hr1tt1kA <68088694+Hr1tt1kA@users.noreply.github.com>
2021-05-28 04:28:36 +0600

    Update module4_solution.js

--- module4_solution.js
+++ module4_solution.js
@@ -1,6 +1,6 @@
 // In this assignment, you are going to loop over an array of names and print out either a hello or goodbye to that name to the browser console. If the name starts with a letter j or J, you are to print out Goodbye JSomeName. If the name starts with any other letter, you are to print out Hello SomeName.
 
-let arrayOfNames = ["Jack","Riya","Julie", "Alexa", "Brake", "Charlie", "David", "Elon", "jake", "jackson"];
+let arrayOfNames = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (element of arrayOfNames) {
     if (element[0] == "j" || element[0] == "J") {
